a914afe7c5495bd412b2b0d0967f6516478fb61f
di.masta <d.simurzin@yandex.ru>
2022-08-22 22:40:53 +0300

    Vebinar

---
+++ Lection/Task12/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число и нажмите Enter");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число и нажмите Enter");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int a = 0;
+
+
+if (firstNumber % secondNumber == 0)
+{
+    Console.WriteLine($"{firstNumber}, {secondNumber} -> кратно");
+}
+else
+{
+    a = firstNumber % secondNumber;
+    Console.WriteLine($"{firstNumber}, {secondNumber} -> не кратно, остаток {a}");
+}
---
+++ Lection/Task16/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число и нажмите Enter");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число и нажмите Enter");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int a = firstNumber * firstNumber;
+int b = secondNumber * secondNumber;
+
+if(firstNumber == b)
+{
+    Console.WriteLine($"{firstNumber}, {secondNumber} -> да");
+}
+else
+{
+    if(secondNumber == a)
+    {
+         Console.WriteLine($"{firstNumber}, {secondNumber} -> да");
+    }
+    else
+    {
+        Console.WriteLine($"{firstNumber}, {secondNumber} -> нет");
+    }
+}
