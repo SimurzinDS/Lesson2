1fbcc73f66b1ce0b80e66353b9741ccf6bf0609b
di.masta <d.simurzin@yandex.ru>
2022-08-22 23:16:13 +0300

    Решил задание №10

--- HomeWork/Task10/Program.cs
+++ HomeWork/Task10/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+int returnSecondNumber(int number)
+{
+    int result = ((number % 100) / 10);
+    return result;
+}
+
+Console.WriteLine("Введите трёхзначное число и нажмите Enter");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int secondNumber = returnSecondNumber(number);
+
+Console.Write($"{number} -> {secondNumber}");
