1a04911046833a79de204facebcf9fd4ef1f2633
di.masta <d.simurzin@yandex.ru>
2022-08-22 20:40:09 +0300

    Task 11

---
+++ Lection/Task11/Program.cs
@@ -0,0 +1,10 @@
+﻿Random random = new Random();
+ // Для генерации рандомных чисел
+int randomNumber = random.Next(100,1000); 
+
+Console.WriteLine(randomNumber);
+
+int lastNumber = randomNumber % 10;
+int firstNumber = randomNumber / 100;
+
+Console.WriteLine($"{firstNumber}{lastNumber}");
