74b139dc408de51ab0b09e4a658a3e7dacbe45ce
di.masta <d.simurzin@yandex.ru>
2022-08-26 02:37:59 +0300

    Решение 15 задачи

--- HomeWork/Task15/Program.cs
+++ HomeWork/Task15/Program.cs
@@ -4,3 +4,27 @@
 // 7 -> да
 // 1 -> нет
 
+Console.WriteLine("Введите число");
+bool isNumber = int.TryParse(Console.ReadLine(), out int x);
+
+
+string GetNumber(int arg)
+{
+    if(arg == 6 && arg == 7)
+    {
+        return "да";
+    }
+    else
+    {
+        if(x < 1 || x > 7)
+        {
+            return "Неверное число";
+        }
+  
+
+    }
+    return "нет";
+}
+
+string result = GetNumber(x);
+Console.WriteLine(result);
