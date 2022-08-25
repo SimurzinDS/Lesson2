9871dcf1ed94d413b245165ce394377a098f27f6
di.masta <d.simurzin@yandex.ru>
2022-08-25 22:58:54 +0300

    Решение 19 задачи

---
+++ HomeWork/HM2/Task19/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+
+string CheckingNumber(string number)
+{
+    if (number[0] == number[4] || number[1] == number[3])
+    {
+        return $"Ваше число: {number} - палиндром.";
+    }
+    else
+    {
+        return $"Ваше число: {number} - НЕ палиндром.";
+    }
+}
+
+if (number!.Length != 5)
+{
+    Console.WriteLine("Введите правильное число");
+    return;
+}
+
+string result = CheckingNumber(number);
+Console.WriteLine(result);
