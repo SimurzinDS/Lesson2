cdc1da5c0ef07df9eb61afbaf8e4f21efdd29bfd
di.masta <d.simurzin@yandex.ru>
2022-08-25 21:19:40 +0300

    Задача 18

---
+++ Lection/Task18/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Введите четверь");
+
+bool isNumber = int.TryParse(Console.ReadLine(), out int x);
+
+if (x<1 || x>4 || isNumber == false)
+{
+    Console.WriteLine("Неверный ввод");
+    return;
+}
+
+string result = GetPosition(x);
+Console.WriteLine("Диапазон равен "+result);
+
+string GetPosition(int quarter)
+{
+    string str1;
+    if(quarter == 1)
+    {
+       str1 = "x > 0 | y >0";
+       return  str1;
+    }
+
+    if(quarter == 2)
+    {
+       str1 = "x < 0 | y > 0";
+       return str1;
+    }
+
+    if(quarter == 3)
+    {
+       str1 = "x < 0 | y < 0";
+       return str1;
+    }
+
+    if(quarter == 4)
+    {
+       str1 = "x > 0 | y < 0";
+       return str1;
+    }
+    
+    return str1 = "Ошибочка какая-то";
+}
