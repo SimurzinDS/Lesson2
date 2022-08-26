4ba3150f4d992d2ae4f1d68e0e554ac7aa60c937
di.masta <d.simurzin@yandex.ru>
2022-08-26 03:42:40 +0300

    Решил 19 задачу

--- HomeWork/HM2/Task19/Program.cs
+++ HomeWork/HM2/Task19/Program.cs
@@ -3,7 +3,7 @@ string? number = Console.ReadLine();
 
 string CheckingNumber(string number)
 {
-    if (number[0] == number[4] || number[1] == number[3])
+    if (number[0] == number[4] && number[1] == number[3])
     {
         return $"Ваше число: {number} - палиндром.";
     }
