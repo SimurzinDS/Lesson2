818c3af3337a47e1d3feea5d087dab8a2ebf0aee
di.masta <d.simurzin@yandex.ru>
2022-08-26 03:03:25 +0300

    Решил 15 задачу

--- HomeWork/Task15/Program.cs
+++ HomeWork/Task15/Program.cs
@@ -4,24 +4,38 @@
 // 7 -> да
 // 1 -> нет
 
-Console.WriteLine("Введите число");
+Console.WriteLine("Введите цифру");
 bool isNumber = int.TryParse(Console.ReadLine(), out int x);
+
+if(isNumber!= true)
+{
+    Console.WriteLine("Нужно вводить цифру");
+    return;
+}
+
+bool isValid = Validate(x);
+
+if (isValid == false)
+{
+    Console.WriteLine("Неверная цифра");
+    return;
+}
+    
+bool Validate(int x)
+{
+    if(x < 1 || x > 7)
+    {
+        return false;
+    }
+    return true;
+}
 
 
 string GetNumber(int arg)
 {
-    if(arg == 6 && arg == 7)
+    if(arg == 6 || arg == 7)
     {
         return "да";
-    }
-    else
-    {
-        if(x < 1 || x > 7)
-        {
-            return "Неверное число";
-        }
-  
-
     }
     return "нет";
 }
